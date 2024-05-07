553a42333a9b6e1854ad5863acc43afc8ae8bf7e
Walter McKelvie <walt@mckelvie.org>
2024-05-07 16:27:28 -0400

    Added commercial laser

--- plot_gaps.py
+++ plot_gaps.py
@@ -10,38 +10,46 @@ import matplotlib
 
 from scipy.interpolate import interp1d
 
-def linfun(x, a):
-    return a*x
-
 if __name__ == "__main__":
     filenames = ["8cm_1", "8cm_2", "11cm_1", "14cm_1", "37cm", "47cm", "56cm"]
+    com_filenames = ["com_laser1", "com_laser2", "com_laser3"]
+    colors = ["g", "r", "b"]
+    linestyles = ["-", "-", "--"]
+
     d = [32, 32, 35, 38, 37, 47, 56]
     dt = []
+
+    com_dt = []
 
     for filename in filenames:
         data = np.genfromtxt(f"output/{filename}_fit.csv", delimiter=",", usecols=range(3), skip_header=1)
         means = data.T[0,:]
         dt.append(np.median(np.diff(means)))
     
+    for filename in com_filenames:
+        data = np.genfromtxt(f"output/{filename}_fit.csv", delimiter=",", usecols=range(3), skip_header=1)
+        means = data.T[0,:]
+        com_dt.append(np.median(np.diff(means)))
+    
     with open(f'output/gaps.csv', 'w') as f:
         print("d,dt", file=f)
         for i in range(0, len(d)):
             print(f"{d[i]},{dt[i]}", file=f)
             print(f"{filenames[i]}:\n\td={d[i]}\n\tdt={dt[i]}\n")
-    
-    fitted_params,_ = scipy.optimize.curve_fit(linfun, dt, d, p0=[1])
-    a = fitted_params[0]
 
-    plt.plot(dt, d, 'o', label="Median Interpeak Distance")
-    xx = np.linspace(np.min(dt), np.max(dt), 2)
-    plt.plot(xx, a*xx, label="Best Fit Line (y=mx)")
 
-    plt.xlabel('Interpeak Distance (s)')
-    plt.ylabel('Distance Between Mirrors (cm)')
 
-    plt.title('Interpeak Distance vs Distance Between Mirrors')
+    plt.plot(d, dt, 'o', label="Interpeak Distance")
 
-    plt.legend(loc="upper left")
+    for i in range(0, len(com_dt)):
+        plt.axhline(y=com_dt[i], linestyle=linestyles[i], color=colors[i], label=f"Commercial Laser Interpeak Distance ({com_filenames[i]})")
+
+    plt.ylabel('Median Interpeak Distance (s)')
+    plt.xlabel('Distance Between Mirrors (cm)')
+
+    plt.title('Distance Between Mirrors vs Median Interpeak Distance')
+
+    plt.legend(loc="upper right")
 
     matplotlib.pyplot.gcf().set_size_inches(18.5, 10.5)
 
